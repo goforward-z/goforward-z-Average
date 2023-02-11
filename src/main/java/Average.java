1375d5a77921683d12d5216543d96d34d25343bb
Lepeng Z <zlepeng.ucf@gmail.com>
2023-02-11 21:19:48 +0000

    done average challenge

--- src/main/java/Average.java
+++ src/main/java/Average.java
@@ -18,6 +18,15 @@ public class Average {
      * @return the average of all the values of nums.
      */
     public double getAvg(int[] nums){
-        return 0;
+
+        int sum = 0;
+
+        for(int i = 0; i < nums.length; i++){
+            sum += nums[i];
+        }
+        
+        double ave = (double) sum / nums.length;
+        
+        return ave;
     }
 }
